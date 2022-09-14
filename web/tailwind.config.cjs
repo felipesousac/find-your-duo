d507f5ff17a99e5ae4ffeb40dcc3f24a3133e36f
felipesousac <claudioflsousa@gmail.com>
2022-09-13 23:51:39 -0300

    Start of static web layout

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient':
+          'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)'
+      }
+    }
+  },
+  plugins: []
+}
