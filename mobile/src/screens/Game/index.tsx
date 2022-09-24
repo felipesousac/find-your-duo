132b79566c6835ea38d9976a15d6777098b7e1b7
felipesousac <claudioflsousa@gmail.com>
2022-09-23 21:16:04 -0300

    Puts logo and bannerImg of each game page

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,18 +1,47 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 import { Background } from '../../components/Background'
+import { Entypo } from '@expo/vector-icons'
 
+import logoImg from '../../assets/logo-nlw-esports.png'
 import { styles } from './styles'
 import { GameParams } from '../../@types/navigation'
+import { TouchableOpacity, View, Image } from 'react-native'
+import { THEME } from '../../theme'
+import { Heading } from '../../components/Heading'
 
 export function Game() {
+  const navigation = useNavigation()
   const route = useRoute()
   const game = route.params as GameParams
-  console.log(game)
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
 
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image source={logoImg} style={styles.logo} />
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+      </SafeAreaView>
     </Background>
   )
 }
