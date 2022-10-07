013415de3338dacc86b797d693b44660665a69e4
felipesousac <claudioflsousa@gmail.com>
2022-10-06 21:47:10 -0300

    Set copy to clipboard function on discord username

--- mobile/src/components/DuoMatch/index.tsx
+++ mobile/src/components/DuoMatch/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import {
+  View,
+  Modal,
+  ModalProps,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator
+} from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { CheckCircle } from 'phosphor-react-native'
+import * as Clipboard from 'expo-clipboard'
 
 import { styles } from './styles'
 import { THEME } from '../../theme'
@@ -13,6 +22,19 @@ interface Props extends ModalProps {
 }
 
 export function DuoMatch({ discord, onClose, ...rest }: Props) {
+  const [isCopying, setIsCopying] = useState(false)
+
+  async function handleCopyDiscordToClipboard() {
+    setIsCopying(true)
+    await Clipboard.setStringAsync(discord)
+
+    Alert.alert(
+      'Discord copiado!',
+      'Usuário copiado para sua área de transferência'
+    )
+    setIsCopying(false)
+  }
+
   return (
     <Modal animationType="fade" transparent statusBarTranslucent {...rest}>
       <View style={styles.container}>
@@ -35,8 +57,18 @@ export function DuoMatch({ discord, onClose, ...rest }: Props) {
 
           <Text style={styles.label}>Adicione no Discord</Text>
 
-          <TouchableOpacity style={styles.discordButton}>
-            <Text style={styles.discord}>{discord}</Text>
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordToClipboard}
+            disabled={isCopying}
+          >
+            <Text style={styles.discord}>
+              {isCopying ? (
+                <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+              ) : (
+                discord
+              )}
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
