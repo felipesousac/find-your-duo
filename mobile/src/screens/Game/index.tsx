8acf4f0c4a5a531bb1829b84bf537895d3bfcc77
felipesousac <claudioflsousa@gmail.com>
2022-10-06 21:29:23 -0300

    Set API call to make the discord username dynamic

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -18,10 +18,16 @@ export function Game() {
   const route = useRoute()
   const game = route.params as GameParams
   const [duos, setDuos] = useState<DuoCardProps[]>([])
-  const [discordDuoSelected, setdiscordDuoSelected] = useState('cloro')
+  const [discordDuoSelected, setdiscordDuoSelected] = useState('')
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.1.26:3333/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => setdiscordDuoSelected(data.discord))
   }
 
   useEffect(() => {
@@ -57,7 +63,7 @@ export function Game() {
           data={duos}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           horizontal
           style={styles.containerList}
@@ -75,7 +81,7 @@ export function Game() {
 
         <DuoMatch
           visible={discordDuoSelected.length > 0}
-          discord="testediscord"
+          discord={discordDuoSelected}
           onClose={() => setdiscordDuoSelected('')}
         />
       </SafeAreaView>
